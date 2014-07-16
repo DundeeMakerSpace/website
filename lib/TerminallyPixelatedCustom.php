092a342fdc6d5d70ffd1182a76528b24220ce197
Terminal Pixel <blether@terminalpixel.co.uk>
2014-07-16 19:51:51 +0100

    add member login and post links

--- lib/TerminallyPixelatedCustom.php
+++ lib/TerminallyPixelatedCustom.php
@@ -9,6 +9,8 @@ class TerminallyPixelatedCustom {
         add_filter( 'timber_context', array( $this, 'donate_page' ) );
         add_action( 'after_setup_theme', array( $this, 'format_support' ) );
         add_action( 'admin_init', array( $this, 'editor_gravity_forms_access' ) );
+        add_filter( 'author_link', array( $this, 'member_link' ), 10, 3);
+        add_filter( 'login_url', array( $this, 'member_login_url' ), 10, 2 );
     }
 
     public static function typekit() { ?>
@@ -28,6 +30,34 @@ class TerminallyPixelatedCustom {
     public static function editor_gravity_forms_access() {
         $role = get_role( 'editor' );
         $role->add_cap( 'gform_full_access' );
+    }
+
+    public static function member_link( $link, $author_id, $author_nicename ) {
+        $members_pages = get_pages( array(
+            'meta_key' => '_wp_page_template',
+            'meta_value' => 'template-members.php',
+            'number' => 1
+        ) );
+        if ( $members_pages ) {
+            $members_link = get_permalink( $members_pages[0]->ID );
+            $link = $members_link . '#' . $author_nicename;
+        }
+        return $link;
+    }
+
+    public static function member_login_url( $login_url, $redirect ) {
+        $login_pages = get_pages( array(
+            'meta_key' => '_wp_page_template',
+            'meta_value' => 'template-login.php',
+            'number' => 1
+        ) );
+        if ( $login_pages ) {
+            $login_url = get_permalink( $login_pages[0]->ID );
+            if ( $redirect ) {
+                $login_url .= '?redirect_to=' . $redirect;
+            }
+        }
+        return $login_url;
     }
 
 }
