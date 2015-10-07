1efbc32fac85af7673140d16684b0c3c105606cf
Grant Richmond <accounts@terminalpixel.co.uk>
2015-10-07 23:05:34 +0100

    show checked in members on the site

--- template-members.php
+++ template-members.php
@@ -16,19 +16,15 @@ if ( !is_singular() ) {
 // $context['members'] = $members;
 
 
-$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id";
-$sqlQuery .= " WHERE mu.membership_id > 0  ";
-$sqlQuery .= " AND mu.status = 'active' ";
-$sqlQuery .= "GROUP BY u.ID ";
-$sqlQuery .= "ORDER BY u.user_registered DESC ";
-$members = $wpdb->get_results( $sqlQuery, 'ARRAY_A' );
+$members = MakerspaceOpen::get_members();
+$member_data = array();
 
 foreach( $members as $key => $member ) {
-    $members[$key]['userdata'] = get_userdata( $member['ID'] );
-    $members[$key]['meta'] = get_user_meta( $member['ID'] );
-    $members[$key]['avatar'] = get_avatar( $member['ID'], 300 );
-    $members[$key]['posts'] = Timber::get_posts( array(
-        'author' => $member['ID'],
+    $member->userdata = get_userdata( $member->ID );
+    $member->meta = get_user_meta( $member->ID );
+    $member->avatar = get_avatar( $member->ID, 300 );
+    $member->posts = Timber::get_posts( array(
+        'author' => $member->ID,
         'tax_query' => array(
             'relation' => 'OR',
             array(
@@ -43,17 +39,19 @@ foreach( $members as $key => $member ) {
             )
         )
     ) );
-    $members[$key]['projects'] = Timber::get_posts( array(
+    $member->projects = Timber::get_posts( array(
         'post_type' => 'projects',
         'connected_type' => 'project_contributors',
-        'connected_items' => $member['ID'],
+        'connected_items' => $member->ID,
         'suppress_filters' => false,
         'nopaging' => true
     ) );
+
+    $member_data[] = $member;
 }
 
-shuffle( $members );
-$context['members'] = $members;
+shuffle( $member_data );
+$context['members'] = $member_data;
 
 
 Timber::render( 'members.twig', $context );
