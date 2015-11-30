2c49e04b278421624dbeb0d76246c91ca4c28e85
Grant Richmond <accounts@terminalpixel.co.uk>
2015-11-30 22:01:40 +0000

    fix showing all resources on archives

--- taxonomy-resource-locations.php
+++ taxonomy-resource-locations.php
@@ -3,10 +3,12 @@ if (!class_exists('Timber')){
     echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
 }
 
+global $wp_query;
+$query = $wp_query->query;
+$query['posts_per_page'] = -1;
+
 $context = Timber::get_context();
-$context['resources'] = Timber::get_posts( array(
-    'posts_per_page' => -1,
-) );
+$context['resources'] = Timber::get_posts( $query );
 $context['term_description'] = term_description();
 
 Timber::render( 'resource-type.twig', $context );
