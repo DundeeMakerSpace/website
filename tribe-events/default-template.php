5f773c7aaee3b539061a40dc19685f1d1bcd434f
Grant Richmond <accounts@terminalpixel.co.uk>
2016-03-21 21:12:15 +0000

    add padding to event archive pages

--- tribe-events/default-template.php
+++ tribe-events/default-template.php
@@ -3,6 +3,7 @@ if (!class_exists('Timber')){
     echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
 }
 $context = Timber::get_context();
+$context['title'] = false;
 // $context['posts'] = Timber::get_posts();
 // if ( !is_singular() ) {
 //     $context['is_archive'] = true;
